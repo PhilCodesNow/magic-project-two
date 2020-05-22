5d959a86d411ea96a9ff4f6aa191c9383c0c00c5
PhilCodesNow <philipmayojr@outlook.com>
2020-05-21 22:26:49 -0400

    mongoDB connected

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 //// Dependencies
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 require('dotenv').config();
+const DBURL = process.env.MONGODB_URI;
 
 ////// Database Connection
+mongoose.connect(DBURL, {}, (err) =>{
+    console.log(err? err : 'connected')
+})
 
 
 ////// Middleware
