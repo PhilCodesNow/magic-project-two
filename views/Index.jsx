58ce5a8e94cd7594c9923d3467a6acad51305eff
PhilCodesNow <philipmayojr@outlook.com>
2020-05-25 18:00:16 -0400

    updated styling

--- views/Index.jsx
+++ views/Index.jsx
@@ -5,38 +5,36 @@ const Layout = require('./components/Layout.jsx');
 
 class Index extends React.Component{
     render(){
-        const {notes} = this.props
-        return(
-            <Layout>
-                <body className="viewsIndex">
-                    <h1 className="page-header">My Notes</h1>
-                    <div className="d-flex justify-content-center">
-                    <a href="/new"><button className="btn btn-success m-3">New Note</button></a>
-                    <a href="/user/new"><button className="btn btn-secondary m-3">Create Account</button></a>
-                    <a href="/sessions/newSession"><button className="btn btn-primary m-3">Log In</button></a>
-                    </div>
+    const {notes} = this.props
+    return(
+        <Layout>
+        <body className="viewsIndex">
+            <h1 className="page-header">My Notes</h1>
+            <div className="d-flex justify-content-center">
+            <a href="/new"><button className="btn btn-success m-3">New Note</button></a>
+            <a href="/user/new"><button className="btn btn-secondary m-3">Create Account</button></a>
+            <a href="/sessions/newSession"><button className="btn btn-primary m-3">Log In</button></a>
+            </div>
 
 
-                    <button type="button" class="btn btn-lg btn-danger mb-3" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
+            <button type="button" class="btn btn-lg btn-danger mb-3" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
 
 
-                    <div className="list-group w-50 m-auto">
-                    {
-                        notes.map((note, index) =>{
-                            return(
-                                <a className="list-group-item list-group-item-action list-group-item-warning fonts" href={`/${note._id}`}>
-                                <h2>{note.title}</h2>
-                                {note.body}
-                                </a>
-                            )
-                        })
-                    }    
-                    </div>
-                
-
-                </body>
-            </Layout>
-        )
+            <div className="list-group w-50 m-auto">
+            {
+                notes.map((note, index) =>{
+                    return(
+                        <a className="list-group-item lgi list-group-item-action list-group-item-warning fonts" href={`/${note._id}`}>
+                        <h2>{note.title}</h2>
+                        {note.body}
+                        </a>
+                    )
+                })
+            }    
+            </div>
+        </body>
+        </Layout>
+    )
     }
 }
 
--- views/Show.jsx
+++ views/Show.jsx
@@ -10,8 +10,8 @@ class Show extends React.Component{
             <Layout>
                 <body className="viewsShow text-center">
                     <div className="list-group-item list-group-item-warning w-50 m-auto">
-                        <h2 className="m-3">{note.title}</h2>
-                        <p>{note.body}</p>
+                        <h2 className="m-3 fonts">{note.title}</h2>
+                        <p className="fonts">{note.body}</p>
                     </div>
                     <div className="d-flex justify-content-center">
                     <a href={`/${note._id}/edit`}><button className="btn btn-warning m-3">Edit</button></a><br/>
